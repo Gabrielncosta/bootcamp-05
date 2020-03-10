1c7c166f075f23ee95b02dfd5dd34af026d00c69
Gabrielncosta <gabrielncosta208@gmail.com>
2020-03-09 22:32:42 -0300

    repositoryPage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
@@ -17,7 +18,7 @@ export default class Main extends Component {
     const repositories = localStorage.getItem('repositories');
 
     if (repositories) {
-      this.setState({ repositories: JSON.parse(repositories)});
+      this.setState({ repositories: JSON.parse(repositories) });
     }
   }
 
@@ -80,7 +81,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
